97fd78d0a266081e77deaf3c644e15eb460690a2
zyegfryed <zyegfryed@gmail.com>
2012-08-30 10:37:11 +0200

    Use regular expression for mobile detection from http://detectmobilebrowsers.com/

--- mobileme/utils.py
+++ mobileme/utils.py
@@ -5,36 +5,10 @@ from .conf import settings
 
 _local = threading.local()
 
-user_agents_test_match = r'^(?:%s)' % '|'.join([
-    "w3c ", "acs-", "alav", "alca", "amoi", "audi",
-    "avan", "benq", "bird", "blac", "blaz", "brew",
-    "cell", "cldc", "cmd-", "dang", "doco", "eric",
-    "hipt", "inno", "ipaq", "java", "jigs", "kddi",
-    "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-",
-    "maui", "maxo", "midp", "mits", "mmef", "mobi",
-    "mot-", "moto", "mwbp", "nec-", "newt", "noki",
-    "xda",  "palm", "pana", "pant", "phil", "play",
-    "port", "prox", "qwap", "sage", "sams", "sany",
-    "sch-", "sec-", "send", "seri", "sgh-", "shar",
-    "sie-", "siem", "smal", "smar", "sony", "sph-",
-    "symb", "t-mo", "teli", "tim-", "tosh", "tsm-",
-    "upg1", "upsi", "vk-v", "voda", "wap-", "wapa",
-    "wapi", "wapp", "wapr", "webc", "winw", "winw",
-    "xda-",
-])
-user_agents_test_search = "(?:%s)" % '|'.join([
-    'up.browser', 'up.link', 'mmp', 'symbian', 'smartphone', 'midp',
-    'wap', 'phone', 'windows ce', 'pda', 'mobile', 'mini', 'palm',
-    'netfront', 'opera mobi',
-])
-user_agents_exception_search = u"(?:%s)" % u'|'.join([
-    'ipad',
-])
-
-http_accept_re = re.compile("application/vnd\.wap\.xhtml\+xml", re.IGNORECASE)
-user_agents_test_match_re = re.compile(user_agents_test_match, re.IGNORECASE)
-user_agents_test_search_re = re.compile(user_agents_test_search, re.IGNORECASE)
-user_agents_exception_search_re = re.compile(user_agents_exception_search, re.IGNORECASE)
+# Ported by Matt Sullivan http://sullerton.com/2011/03/django-mobile-browser-detection-middleware/
+# Credits: http://detectmobilebrowsers.com/
+reg_b = re.compile(r"android.+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|meego.+mobile|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino", re.I | re.M)
+reg_v = re.compile(r"1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|sk\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-", re.I | re.M)
 
 
 def get_flavour_from_request(request):
@@ -42,8 +16,6 @@ def get_flavour_from_request(request):
     Analyzes the request to find what flavour the user wants the system to
     show. Only flavours listed in settings.FLAVOURS are taken into account.
     """
-    from mobileme.conf import settings
-
     if hasattr(request, 'session'):
         flavour = request.session.get(settings.FLAVOURS_SESSION_NAME, None)
     else:
@@ -56,22 +28,10 @@ def get_flavour_from_request(request):
     if 'HTTP_USER_AGENT' in request.META:
         user_agent = request.META['HTTP_USER_AGENT']
 
-        # Test common mobile values.
-        if user_agents_test_search_re.search(user_agent) and \
-            not user_agents_exception_search_re.search(user_agent):
+        b = reg_b.search(user_agent)
+        v = reg_v.search(user_agent[0:4])
+        if b or v:
             is_mobile = True
-        else:
-            # Nokia like test for WAP browsers.
-            # http://www.developershome.com/wap/xhtmlmp/xhtml_mp_tutorial.asp?page=mimeTypesFileExtension
-            if 'HTTP_ACCEPT' in request.META:
-                http_accept = request.META['HTTP_ACCEPT']
-                if http_accept_re.search(http_accept):
-                    is_mobile = True
-
-        if not is_mobile:
-            # Now we test the user_agent from a big list.
-            if user_agents_test_match_re.match(user_agent):
-                is_mobile = True
 
     return is_mobile and settings.DEFAULT_MOBILE_FLAVOUR or settings.FLAVOURS[0]
 
